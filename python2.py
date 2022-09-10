6c1638ade78d1205022688bec2a451670fea3a23
Roman <Yuuziklol@gmail.com>
2022-09-10 17:14:22 +0700

    задача 5.2

--- python2.py
+++ python2.py
@@ -48,3 +48,14 @@
 # import random
 # random.shuffle(list)
 # print(list)
+
+# list = [5,8,2,3,15,90,52,0]
+# import random
+# i = 0
+# while i < len(list):
+#     second_pos = random.randint(0,7)
+#     help = list[i]
+#     list[i] = list[second_pos]
+#     list[second_pos] = help
+#     i += 1
+# print(list)
